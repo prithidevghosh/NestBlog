1a1a86a98bc0f68cb42230c271143d3ce00a8762
Prithidev Ghosh <Prithidevghosh@gmail.com>
2023-03-23 23:33:01 +0530

    final changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello prithvi!';
+    return 'This a Blogging website made using Nest';
   }
 }
