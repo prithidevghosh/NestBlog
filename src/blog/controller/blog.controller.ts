5be9e214e18a71479c13148d4b45cb9de4a672b7
Prithidev Ghosh <Prithidevghosh@gmail.com>
2023-03-23 20:32:46 +0530

    added blog pagination

--- src/blog/controller/blog.controller.ts
+++ src/blog/controller/blog.controller.ts
@@ -5,7 +5,7 @@ import { userIsAuthor } from '../guard/user-is-author.guard';
 import { BlogEntry } from '../model/blog-entry.interface';
 import { BlogService } from '../service/blog.service';
 
-@Controller('blogs')
+@Controller('blog-entries')
 export class BlogController {
     constructor (private blogservice:BlogService){};
 
@@ -16,18 +16,47 @@ export class BlogController {
         return this.blogservice.create(user,blogentry);
      }
 
-     @Get()
-     findBlogEntries(
-        @Query('userid')userid:number
-     ):Observable<BlogEntry[]>{
+    //  @Get()
+    //  findBlogEntries(
+    //     @Query('userid')userid:number
+    //  ):Observable<BlogEntry[]>{
         
-        if(userid==null){
-         return this.blogservice.findAll();
-        }else{
-          return from(this.blogservice.findByUser(userid));
-        }
-     }
+    //     if(userid==null){
+    //      return this.blogservice.findAll();
+    //     }else{
+    //       return from(this.blogservice.findByUser(userid));
+    //     }
+    //  }
      
+    @Get('')
+    index(
+        @Query('page')page:number=1,
+        @Query('limit')limit:number=2
+    ){
+        limit = limit > 100 ? 100 : limit;
+        return this.blogservice.paginateAll({
+            page:Number(page)
+            ,limit:Number(limit),
+            route:" http://localhost:3000/blog-entries"
+            
+        })
+    }
+
+    @Get('user/:user')
+    indexByUser(
+        @Query('page')page:number=1,
+        @Query('limit')limit:number=2,
+        @Param('user')userID:number
+    ){
+        limit = limit > 100 ? 100 : limit;
+        return this.blogservice.paginateAllByUser({
+            page:Number(page)
+            ,limit:Number(limit),
+            route:" http://localhost:3000/blog-entries"
+            
+        },Number(userID))
+    }
+
      @Get(':id')
      findOne(@Param('id')id:number):Observable<BlogEntry>{
         return from(this.blogservice.findOne(id));
--- src/blog/service/blog.service.ts
+++ src/blog/service/blog.service.ts
@@ -1,64 +1,98 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
 import { from, map, Observable, of, switchMap } from 'rxjs';
 import { User } from 'src/user/models/user.interface';
 import { UserService } from 'src/user/service/user.service';
 import { Repository } from 'typeorm';
 import { BlogEntryEntity } from '../model/blog-entry.entity';
 import { BlogEntry } from '../model/blog-entry.interface';
-const slugify=require('slugify');
+const slugify = require('slugify');
 @Injectable()
 export class BlogService {
 
- constructor(
-    @InjectRepository(BlogEntryEntity) private readonly blogRepository:Repository<BlogEntryEntity>,
-    private userservice:UserService 
- ){}
+    constructor(
+        @InjectRepository(BlogEntryEntity) private readonly blogRepository: Repository<BlogEntryEntity>,
+        private userservice: UserService
+    ) { }
 
- create(user:User,blogentry:BlogEntry):Observable<BlogEntry>{
-     blogentry.author=user;
-     return this.generateSlug(blogentry.title).pipe(
-        switchMap((slug:string)=>{
-            blogentry.slug=slug;
-            return from(this.blogRepository.save(blogentry));
-        })
-     )
- }
+    create(user: User, blogentry: BlogEntry): Observable<BlogEntry> {
+        blogentry.author = user;
+        return this.generateSlug(blogentry.title).pipe(
+            switchMap((slug: string) => {
+                blogentry.slug = slug;
+                return from(this.blogRepository.save(blogentry));
+            })
+        )
+    }
 
- generateSlug(title:string):Observable<string>{
-    return of(slugify(title));
- }
+    generateSlug(title: string): Observable<string> {
+        return of(slugify(title));
+    }
 
- findAll():Observable<BlogEntry[]>{
-     return from(this.blogRepository.find({relations:['author']}))
- }
+    findAll(): Observable<BlogEntry[]> {
+        return from(this.blogRepository.find({ relations: ['author'] }))
+    }
 
- findByUser(userId: number): Observable<BlogEntry[]> {
-    return this.userservice.findOne(userId).pipe(
-      switchMap(user => {
-        return from(this.blogRepository.find({
-          where: {
-            author: user
-          },
-          relations: ['author']
-        }));
-      }),
-      map((blogEntries: BlogEntry[]) => blogEntries)
-    );
-  }
+    findByUser(userId: number): Observable<BlogEntry[]> {
+        return this.userservice.findOne(userId).pipe(
+            switchMap(user => {
+                return from(this.blogRepository.find({
+                    where: {
+                        author: user
+                    },
+                    relations: ['author']
+                }));
+            }),
+            map((blogEntries: BlogEntry[]) => blogEntries)
+        );
+    }
 
-  findOne(id:number):Observable<BlogEntry>{
-    return from(this.blogRepository.findOne({ where: { id }, relations: ['author'] }));
-  }
+    findOne(id: number): Observable<BlogEntry> {
+        return from(this.blogRepository.findOne({ where: { id }, relations: ['author'] }));
+    }
 
-  updateOne(id:number,blogentry:BlogEntry):Observable<BlogEntry>{
-    return from(this.blogRepository.update(id,blogentry)).pipe(
-        switchMap(()=>this.findOne(id))
-    )
-  }
+    updateOne(id: number, blogentry: BlogEntry): Observable<BlogEntry> {
+        return from(this.blogRepository.update(id, blogentry)).pipe(
+            switchMap(() => this.findOne(id))
+        )
+    }
 
 
-   deleteOne(id:number):Observable<any>{
-    return from(this.blogRepository.delete(id));
-   }
+    deleteOne(id: number): Observable<any> {
+        return from(this.blogRepository.delete(id));
+    }
+
+    paginateAll(options: IPaginationOptions): Observable<Pagination<BlogEntry>> {
+        return from(paginate<BlogEntryEntity>(
+            this.blogRepository, // pass the repository instance here
+            options,
+            { relations: ['author'] }
+        )).pipe(
+            map((blogentries: Pagination<BlogEntry>) => blogentries)
+        );
+    }
+
+    paginateAllByUser(options: IPaginationOptions, userID: number): Observable<Pagination<BlogEntry>> {
+        return this.userservice.findOne(userID).pipe(
+          switchMap(user => {
+            return from(paginate<BlogEntryEntity>(
+              this.blogRepository, // pass the repository instance here
+              options,
+              {
+                relations: ['author'],
+                where: [
+                  { author: user }
+                ]
+              }
+            )).pipe(
+              map((blogentries: Pagination<BlogEntry>) => blogentries)
+            );
+          })
+        );
+      }
+      
+
+
 }
+
