042406ca05a2eeff5af919b9f911a06881193c4e
Prithidev Ghosh <Prithidevghosh@gmail.com>
2023-03-24 19:26:11 +0530

    changed h1 in app.services

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1>This a Blogging website made using Nest<h1>, run the apis given in postman file here / in postman app to see features';
+    return '<h1>This a Blogging website made using Nest<h1> Run the apis given in postman file here / in postman app to see features';
   }
 }
